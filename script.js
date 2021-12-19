474f2b44b4103baf6d406049031ea8f3e5dcf889
AnasJMirza <ajmhimself@gmail.com>
2021-12-19 15:59:36 +0500

    One question done

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function reminder() {
     rem = number
     document.getElementById("reminder").innerHTML = `Reminder = ${rem}`
 }
+
+
+// Question - 02 : replace a word from string withpout using javascript replace() function
+
+
+function replace() {
+
+    var string = document.getElementById('string').value;
+    var toReplace = document.getElementById('toReplace').value;
+    var replaceWith = document.getElementById('replaceWith').value;
+
+    document.getElementById('old').innerHTML = `Old String : ${string}`
+
+    
+    for (let index = 0; index < string.length; index++) {
+        if (string.slice(index, index + toReplace.length) == toReplace) {
+            var previousString = string.slice(0, index - 1);
+            var nextString = string.slice(index + toReplace.length + 1, string.length)
+            var newString = previousString + " " + replaceWith + " " + nextString;
+            document.getElementById('result-02').innerHTML = `New String : ${newString}`
+        }
+        
+    }
+}
